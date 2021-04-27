b78e12d13aeb1443684db3d4f73d7cdb53fef1cf
Kevin Kurian T <kevinthathathil@gmail.com>
2021-04-27 15:19:58 +0530

    added singly linked list code
    This file contains code for performing various linked list operations

---
+++ singly_linked_list.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node 
+{
+    int data;
+    struct node* next;
+};
+
+struct node* head=NULL;
+void insertathead(int x )
+{
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=x;
+    temp->next=head;
+    head=temp;
+
+}
+
+void insert_at_end(int x)
+{   struct node* temp=(struct node*)malloc(sizeof(struct node));
+   
+   if(head==NULL)
+   {
+        temp->data=x;
+        temp->next=head;
+       head=temp;
+       return;
+   } 
+    temp->data=x;
+    temp->next=NULL;
+   struct node* ptr=head;
+   while(ptr->next!=NULL)
+   {
+       ptr=ptr->next;
+
+   }
+    ptr->next=temp;
+}
+
+void print()
+{
+    struct node* temp=head;
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+    printf("\n");
+}
+
+void insert_at_n(int x,int n)
+{
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=x;
+    temp->next=NULL;
+    if(n==1)
+    {
+        temp->next=head;
+        head=temp;
+        return;
+    }
+    struct node* temp1;
+    temp1=head;
+    for(int i=0;i<n-2;i++)
+    {
+        temp1=temp1->next;
+
+    }
+    temp->next=temp1->next;
+    temp1->next=temp;
+
+}
+void delete_nthlink(int n)
+{
+    int i;
+    struct node* temp1=head;
+    for(i=0;i,n-2;i++)
+    {
+        temp1=temp1->next;
+
+    }
+    struct node* temp2=temp1->next;
+    temp1->next=temp2->next;
+    free(temp2);
+}
+
+int  main()
+{
+    insertathead(9);    print();
+    insert_at_end(92);print();
+    insert_at_n(123,2);print();
+    insertathead(7);    
+    print();
+    delete_nthlink(2);
+    print();
+    return 0;
+}
